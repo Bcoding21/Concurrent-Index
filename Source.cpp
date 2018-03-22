4cf3731bd476ce02a408f490dfc8b670726a3d01
Brandon <brandon.cole@bison.howard.edu>
2018-03-22 01:52:44 -0400

    Added comments. Renamed vars

--- Source.cpp
+++ Source.cpp
@@ -1,48 +1,47 @@
 #include "BlockIndexer.h"
 
-namespace fs = std::experimental::filesystem::v1;
-
 void setSections(std::vector<std::vector<std::string>>&, const std::vector<std::string>&, int, int);
 
 int main() {
 
 	std::vector<std::string> directories;
 
-	std::transform(
+	std::transform( // add sub directories to list
 		fs::directory_iterator("pa1-data"),
 		fs::directory_iterator(),
 		std::back_inserter(directories),
 		[](const auto& entry) {
-		return fs::path(entry).string();
-	});
+		return fs::path(entry).string(); }
+	);
 
-	std::vector<std::vector<std::string>> sections;
+	std::vector<std::vector<std::string>> sections; 
 	int maxThreads = std::thread::hardware_concurrency();
-	setSections(sections, directories, maxThreads, 0);
+	setSections(sections, directories, maxThreads, 0); // split directories into sections
+													   // section size depends on num cores
 
-	short directoryCount = directories.size();
-	short dirIndex = 0;
+	short directoriesLeft = directories.size();
+	short j = 0;
 
-	while (directoryCount) {
+	while (directoriesLeft) {
 
-		short numThreads = (directoryCount >= maxThreads) ? maxThreads : directoryCount;
-		directoryCount -= numThreads;
+		short numThreads = (directoriesLeft >= maxThreads) ? maxThreads : directoriesLeft;
+		directoriesLeft -= numThreads;
 
 		std::vector<BlockIndexer> indexers(numThreads - 1);
 
 		std::vector<std::thread> threads;
-		for (int i = 0; i < numThreads - 1; i++) {
+		for (int i = 0; i < numThreads - 1; i++) { // index files in directories on separate threads
 			threads.push_back(
 				std::move(
 				std::thread(
 				&BlockIndexer::index,
 				std::ref(indexers[i]),
-				std::ref(directories[dirIndex++])
+				std::ref(directories[j++])
 				)));
 		}
 
-		BlockIndexer indexer;
-		indexer.index(directories[dirIndex++]);
+		BlockIndexer indexer; // index 1 directory in main thread
+		indexer.index(directories[j++]);
 
 		for (auto& thread : threads) { thread.join(); }
 	}
